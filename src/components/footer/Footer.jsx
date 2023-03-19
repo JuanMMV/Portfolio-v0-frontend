9bd574adaaebb867b5df9c8aa4ac89695e8034ee
JuaMV <juan.menares.vega@ciisa.cl>
2023-03-19 02:25:54 -0300

    Cambio de colores y actualizacion de footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './footer.css'
-import { FaFacebookF } from 'react-icons/fa'
-import { FiInstagram } from 'react-icons/fi'
-import { IoLogoTwitter } from 'react-icons/io'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
 	return (
 		<footer>
-			<a href="#" className='footer__logo'>EGATOR</a>
+			<a href="#" className='footer__logo'>Juan Menares</a>
 			<ul className='permalinks'>
 				<li><a href="#">Home</a></li>
 				<li><a href="#about">About</a></li>
@@ -19,13 +18,12 @@ const Footer = () => {
 			</ul>
 
 			<div className="footer__socials">
-				<a href="https://facebook.com"><FaFacebookF /></a>
-				<a href="https://instagram.com"><FiInstagram /></a>
-				<a href="https://twitter.com"><IoLogoTwitter /></a>
+				<a href='https://www.linkedin.com/in/juan-menares-vega-ba29b61a3/'><BsLinkedin /></a>
+				<a href='https://github.com/JuanMMV'><FaGithub /></a>
 			</div>
 
 			<div className="footer__copyright">
-				<small>&copy; EGATOR tutorials. All rights reserved</small>
+				<small>活 Juan Menares portfolio</small>
 			</div>
 		</footer>
   	)
